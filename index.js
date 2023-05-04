9204f69b0d78fe38a354ecd7890db30e770a8e9d
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-05-04 17:37:28 +0600

    chef json make and show in locahost

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+
+const chef = require(`./chef.json`)
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/chef', (req, res) => {
+  res.send(chef)
+})
+
+app.get('/chef/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  const selectedChef = chef.find(chef => parseInt(chef.id) === id)
+  res.send(selectedChef)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
